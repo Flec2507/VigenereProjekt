aeda7a9b2f3eb51ffd1ad5c277275c6abc37dae8
Flec2507 <pietbeineke@gmail.com>
2022-05-25 18:58:49 +0200

    0.1

--- src/main.java
+++ src/main.java
@@ -14,47 +14,59 @@ public class main {
         encode(getInput("input"), getInput("key"));
     }
 
-    public static void encode(String key, String input) {
-        System.out.println(key + " " + input);
-        if(key.length() != userInputLength){//wir versuchen den key lang genug zu machen schmerzen!
-            StringBuilder builder = new StringBuilder(input.length());
-            while (builder.length() < input.length()) {
-                builder.append(key.substring(0, Math.min(key.length(), builder.length() - input.length())));
+    public static void encode(String input, String key) {
+        System.out.println("Key: " + key + ", Text: " + input);
+        if (key.length() != userInputLength) {//Resize the Key to the same length as the Text for easy encoding
+            for (; ; ) {
+                boolean breaker = false;
+                for (int i = 0; i < key.length(); i++) {
+                    if (repeatingKey.size() < userInputLength) {
+                        repeatingKey.add(key.charAt(i));
+                    } else {
+                        System.out.println(input);
+                        breaker = true;
+                        break;
+                    }
+                }
+                if (breaker) {
+                    break;
+                }
             }
-            String result = builder.toString();
-            System.out.println(result);
+        }
+        for (int i = 0; i < repeatingKey.size(); i++) {//Print the key for clarity
+            System.out.print(repeatingKey.get(i));
         }
     }
 
-    public static String getInput(String use) {
+    public static String getInput(String use) {//Get stuff from the User
         Scanner scanner = new Scanner(System.in);
         String userInput = "";
         String userKey = "";
-        if (use.equals("input")) {
+        if (use.equals("input")) {//When we want the Input we call this
             System.out.println("Bitte geben Sie ihren Text ein!");
             for (; ; ) {
                 userInput = "";
                 userInput = scanner.nextLine();
-                if (!Pattern.matches("[a-zA-Z]+", userInput)) {
-                    System.out.println("Ich bin leider nicht in der Lage irgendetwas ab von Buchstaben zu verstehen... Bitte versuchen Sie es erneut!");
-                } else {
+                if (!Pattern.matches("[a-zA-Z]+", userInput)) {//If the Text contains anything else than letters it is unusable
+                    System.out.println("Ich bin leider nicht in der Lage irgendetwas abseits von Buchstaben zu verstehen... Bitte versuchen Sie es erneut!");
+                } else {//Everything should work with this
                     System.out.println("Pure Buchstaben");
                     break;
                 }
             }
-            userInputLength = userInput.length();
+            userInputLength = userInput.length();//We need a global variable for this because the if statement in the Key-Case is in a separate call of the function
             return userInput;
-        } else if (use.equals("key")) {
+        } else if (use.equals("key")) {//Same thing
             System.out.println("Bitte geben Sie ihren Schlüssel ein!");
             for (; ; ) {
                 userKey = "";
                 userKey = scanner.nextLine();
-                if (!Pattern.matches("[a-zA-Z]+", userKey)) {
-                    System.out.println("Ich bin leider nicht in der Lage irgendetwas ab von Buchstaben zu verstehen... Bitte versuchen Sie es erneut!");
+                if (!Pattern.matches("[a-zA-Z]+", userKey)) {//Same thing
+                    System.out.println("Ich bin leider nicht in der Lage irgendetwas abseits von Buchstaben zu verstehen... Bitte versuchen Sie es erneut!");
                 } else {
-                    if(userKey.length() > userInputLength){
-                        System.out.println("Ihr Key ist länger als ihr Input, das funktioniert nicht. Bitte versuchen Sie es erneut!");
-                    }else {
+                    if (userKey.length() > userInputLength) {//A Key longer than the Text is not useful
+                        System.out.println("Ihr Key ist länger als Ihr Input. Das funktioniert hier so nicht. Bitte versuchen Sie es erneut!");
+                    } else {//Same thing
                         System.out.println("Pure Buchstaben");
                         break;
                     }
@@ -62,7 +74,7 @@ public class main {
             }
             return userKey;
         } else {
-            return "ERROR";
+            return "ERROR";//If none of the two programmed use-cases is called, which will never happen
         }
     }
 }
