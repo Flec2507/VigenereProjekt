2b46cba5653aeb497d68f449d247ce55c63b7f4b
Flec2507 <pietbeineke@gmail.com>
2022-05-28 18:28:40 +0200

    0.3

--- src/main.java
+++ src/main.java
@@ -21,12 +21,11 @@ public class main {
         System.out.println("Key: " + key + ", Text: " + input);
         if (key.length() != userInputLength) {//Resize the Key to the same length as the Text for easy encoding
             for (; ; ) {
-                boolean breaker = false;
-                for (int i = 0; i < key.length(); i++) {
-                    if (repeatingKey.size() < userInputLength) {
+                boolean breaker = false;//Helper bool for stopping the for(;;) loop
+                for (int i = 0; i < key.length(); i++) {//Go through the key and add the letters one after the other
+                    if (repeatingKey.size() < userInputLength) {//As long as the Key's length is smaller than the Text's length, add letters
                         repeatingKey.add(key.charAt(i));
-                    } else {
-                        System.out.println(input);
+                    } else {//When they are the same length, stop everything
                         breaker = true;
                         break;
                     }
@@ -36,14 +35,39 @@ public class main {
                 }
             }
         }
+        System.out.println(input);//Print the Text for clarity
         for (int i = 0; i < repeatingKey.size(); i++) {//Print the key for clarity
             System.out.print(repeatingKey.get(i));
         }
+        System.out.println("");//Switch to next line
         for (int i = 0; i < userInputLength; i++) {//Encoding
             char A = input.charAt(i);//Letter at the position of the cycle in the input word
             char B = repeatingKey.get(i);//Letter at the position of the cycle in the key word
             char C = ' ';//Letter after encoding
+            int D = 0;//Position of Key-Letter in alphabet
+            int E = 0;//Amount of movement
+            D = search(B);//See at definition
+            E = search(A);
+            E += D;//Set the letter plus the amount of revolving in the grid
+            if (E > 25) {//If out of bounds, wrap around
+                E = E - 25;
+            }
+            C = alphabet[E];
+            System.out.print(C);
         }
+        System.out.println();//Switch to next line
+    }
+
+    public static int search(char letter) {//Gives me the position of letter requested in the Alphabet
+        int position = 0;
+        for (int i = 0; i < alphabet.length; i++) {
+            position = 0;
+            if (alphabet[i] == letter) {
+                position = i;
+                break;
+            }
+        }
+        return position;
     }
 
     public static String getInput(String use) {//Get stuff from the User
